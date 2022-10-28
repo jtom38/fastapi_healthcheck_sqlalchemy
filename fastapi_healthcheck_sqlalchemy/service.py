f4a7666ab548de0d6959c3c4b3ad85a34e13d8e0
Paul van Eijden <paul@unicornify.com>
2022-10-28 16:08:54 +0200

    column names as text have to be surrounded with literal_column()

--- fastapi_healthcheck_sqlalchemy/service.py
+++ fastapi_healthcheck_sqlalchemy/service.py
@@ -4,7 +4,7 @@ from fastapi_healthcheck.domain import HealthCheckInterface
 from fastapi_healthcheck.enum import HealthCheckStatusEnum
 from fastapi_healthcheck.service import HealthCheckBase
 from fastapi_sqlalchemy import db
-from sqlalchemy import text, exc
+from sqlalchemy import text, exc, literal_column
 
 
 class HealthCheckSQLAlchemy(HealthCheckBase, HealthCheckInterface):
@@ -20,7 +20,7 @@ class HealthCheckSQLAlchemy(HealthCheckBase, HealthCheckInterface):
         res: HealthCheckStatusEnum = HealthCheckStatusEnum.UNHEALTHY
         with db():
             try:
-                db.session.query("ping").from_statement(text("SELECT 1 as ping")).one()
+                db.session.query(literal_column("ping")).from_statement(text("SELECT 1 as ping")).one()
                 res = HealthCheckStatusEnum.HEALTHY
             except exc.DBAPIError:
                 pass
